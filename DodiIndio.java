f930dd560148f7d376fe70395721cfc5f3777945
ShackerWealthy <64730919+ShackerWealthy@users.noreply.github.com>
2020-06-19 12:45:17 +0530

    Modifying the web browser part - 1

--- DodiIndio.java
+++ DodiIndio.java
@@ -1,94 +1,73 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
 
 public class DodiIndio extends Application {
 
+    TextField textField = new TextField();
+    Button loadButton = new Button("Load");
+    Button reloadButton = new Button("Reload");
+    WebView webView;
+    WebEngine engine;
+
     @Override
-    public void start(Stage primaryStage) {
-        MessageBox object = new MessageBox();
+    public void start(Stage primaryStage) throws Exception{
+        MessageBox choose = new MessageBox();
+        choose.askUsersChoice();
 
-        object.showMessage("Disclaimer", "You can only visit a single website using this browser when you launch " +
-                "this browser. If you want\n to visit another website after visiting the first website then you have " +
-                "reopen the web browser.\nAnd if you face any problem then you can't complain me.");
-
-        if (object.hasDisagreed == false) {
-            WebView webView = new WebView();
-            WebEngine engine;
-
+        if (choose.shouldClose == false) {
+            webView = new WebView();
             engine = webView.getEngine();
 
-            TextField textField = new TextField("");
-            textField.setPromptText("Type the URL of the website that you want to visit");
-            textField.setPrefWidth(325);
+            textField.setPromptText("Type in the URL of the website that you want to visit");
+            textField.setPrefWidth(500);
+            textField.setPrefHeight(35);
+            textField.getStyleClass().add("textfield");
 
-            Button buttonLoad = new Button("Load");
-            Button buttonLoadGoogle = new Button("Load Google");
+            loadButton.setPrefWidth(100);
+            loadButton.setPrefHeight(35);
+            reloadButton.setPrefWidth(100);
+            reloadButton.setPrefHeight(35);
 
-            buttonLoad.setPrefWidth(162);
-            buttonLoadGoogle.setPrefWidth(163);
+            loadButton.setOnAction(e -> engine.load(textField.getText()));
 
-            buttonLoad.setOnAction(e -> {
-                if (textField.getText() != "") {
-                    urlLoader(primaryStage, textField.getText(), webView, engine, false);
-                } else {}
-            });
+            reloadButton.getStyleClass().add("button-blue");
 
-            buttonLoadGoogle.setOnAction(e -> {
-                urlLoader(primaryStage, "https://www.google.com", webView, engine, false);
-            });
+            VBox root = new VBox();
+            HBox hbox = new HBox();
 
-            GridPane layout = new GridPane();
+            hbox.getChildren().addAll(textField, loadButton, reloadButton);
+            root.getChildren().addAll(hbox, webView);
 
-            GridPane.setConstraints(textField, 0, 0);
-            GridPane.setColumnSpan(textField, 3);
-            GridPane.setConstraints(buttonLoad, 0, 1);
-            GridPane.setConstraints(buttonLoadGoogle, 2, 1);
+            try {
+                engine.load(choose.url);
+            } catch (IllegalArgumentException e) {
+                engine.loadContent("<html><body><p align=\"CENTER\">Sorry I can't find the link.</p></body></html>");
+            }
 
-            layout.getChildren().addAll(textField, buttonLoad, buttonLoadGoogle);
+            reloadButton.setOnAction(e -> engine.reload());
 
-            Scene scene = new Scene(layout, 325, 55);
+            Scene scene = new Scene(root, 1000, 500);
+            scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 
-            primaryStage.setTitle("Web Browser");
+            primaryStage.setTitle("Advanced Web Browser");
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
             primaryStage.show();
         }
 
-        else {
-            primaryStage.close();
-        }
+        else {}
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args)
+    {
         launch(args);
-    }
-
-    public void urlLoader(Stage primaryStage, String url, WebView webView, WebEngine engine, boolean shouldReload) {
-        engine = webView.getEngine();
-
-        Stage browserStage = new Stage();
-
-        Pane layout = new Pane();
-        layout.getChildren().add(webView);
-
-        engine.load(url);
-
-        if (shouldReload) {
-            engine.reload();
-        }
-
-        browserStage.setTitle("Web Browser");
-        browserStage.setScene(new Scene(layout, 800, 500));
-        browserStage.show();
-        browserStage.setResizable(false);
-
-        primaryStage.close();
     }
 }
